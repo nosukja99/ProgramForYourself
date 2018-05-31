d447296e6a7d7738c347de286215a920a5b2371c
nosukja99 <nosukja99@gmail.com>
2018-05-31 11:45:28 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("My name is Hyejeong Kim");
+        System.out.println("My age is 38");
+        System.out.println("I am  learning Java programming");
+    }
+}
